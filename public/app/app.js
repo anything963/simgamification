d546db775360f65d6804a5f9bf07246b241fa751
Shubha Shrestha <suba.stha@hotmail.com>
2016-07-16 23:09:36 -0500

    Leveling system

--- public/app/app.js
+++ public/app/app.js
@@ -5,6 +5,22 @@
 (function(){
     "use strict";
     
-    angular.module('simgamification', []);
+    var app = angular.module('simgamification',
+                    ["ngAnimate",
+                        "ui.bootstrap",
+                        "countTo"]);
     
+    app.controller("homeController",["$timeout", homeController] );
+    
+    function homeController($timeout){
+        var vm = this;
+        var currentValue = 100;
+        vm.progressValue = 0;
+        vm.countTo = currentValue;
+        vm.countFrom = 0;
+
+        $timeout(function(){
+            vm.progressValue = currentValue;
+        }, 200);
+    }
 })();
--- resources/views/index.php
+++ resources/views/index.php
@@ -2,8 +2,10 @@
 <html lang="en" ng-app="simgamification">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Google Phone Gallery</title>
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="bower_components/angular-bootstrap/ui-bootstrap-csp.css" />
     <link rel="stylesheet" href="app/app.css" />
 
     <script src="bower_components/jquery/dist/jquery.js"></script>
@@ -12,10 +14,13 @@
     <script src="bower_components/angular-animate/angular-animate.js"></script>
     <script src="bower_components/angular-resource/angular-resource.js"></script>
     <script src="bower_components/angular-route/angular-route.js"></script>
+    <script src="bower_components/angular-bootstrap/ui-bootstrap.js"></script>
+    <script src="bower_components/angular-bootstrap/ui-bootstrap-tpls.js"></script>
+    <script src="lib/angular-count-to/angular-count-to.js"></script>
     <script src="app/app.js"></script>
 
 </head>
-<body>
+<body >
 
 <!-- Navbar -->
 <nav class="navbar navbar-default">
@@ -39,12 +44,35 @@
 </nav>
 
 <!-- Level System -->
-<div class="container">
-    <div class="leveling" class="text-center">
-        <h2>Leveling System</h2>
-        <h4>Current level and experienct points goes here.</h4>
+<div class="container" ng-controller="homeController as vm">
+    <div class="leveling">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="image-wrapper">
+                    <div class="absolute-center image-container ">
+                        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSmrZDL7TAeq1OHftaRTHnoQxUB93EMYWc8qSa-2eIL1EPEuHq4JA" alt=""/>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-9">
+                <div class="progressbar-wrapper">
+                    <div class="progressbar-container absolute-center">
+                        <uib-progressbar class="progress-striped" animate="true" max="250" value="vm.progressValue" type="success">
+                            <i>
+                                <span count-to="{{vm.countTo}}" duration=".75" count-from="{{vm.countFrom}}"></span> / 250
+                            </i>
+                        </uib-progressbar>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
 </div>
+
+
 
 <!-- Container (Other gamification Section) -->
 <div id="other-gamification" class="container">
